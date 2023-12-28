a54739f3ada83bc086aa84adb34e4f5e8260291f
dev-santi <bauzalarrosasantiago@gmail.com>
2023-12-28 09:39:46 -0300

    guia de utilizacion

--- src/app/acerca-de/page.tsx
+++ src/app/acerca-de/page.tsx
@@ -3,14 +3,32 @@ export default function page() {
         <main className='py-12 gap-5 w-fit flex flex-col min-h-[calc(100vh-5rem)] max-w-[45rem] text-white'>
             <h1 className='font-bold text-2xl uppercase mt-10'>Objetivo y guia de utilización</h1>
             <p>
-                La aplicación tiene como objetivo principal el poder facilitar una gestión eficiente de los
-                plazos legales, permitiendo calcular con precisión las fechas de término optimizando la
-                planificación de tareas.
+                La aplicación tiene como objetivo principal el facilitar una gestión eficiente de los plazos,
+                permitiendo calcular con precisión las fechas término optimizando así la planificación de
+                tareas.
             </p>
             <p>
-                La aplicación se apoya en calendarios suministrados por instituciones académicas y
+                La aplicación se apoya en calendarios proporcionados por instituciones académicas y
                 profesionales, asegurando la máxima precisión al tener en cuenta eventos y días hábiles
-                específicos relevantes para la práctica legal.
+                específicos relevantes para la práctica jurídica.
+            </p>
+            <p>
+                <span className='text-red-400'>¿Cómo se utiliza la aplicación?</span>
+                <br /> <br />
+                <ol className='flex flex-col gap-4 list-decimal'>
+                    <li>
+                        <h2>Seleccione la opción correspondiente a su caso:</h2>
+                        <ol className='ml-10 mt-2' type='a'>
+                            <li>Derecho Laboral: Es el filtro por defecto al ingresar al sitio, </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h2>-</h2>
+                    </li>
+                    <li>
+                        <h2>-</h2>
+                    </li>
+                </ol>
             </p>
         </main>
     );
